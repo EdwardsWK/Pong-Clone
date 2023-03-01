89dca8b60cadc3b5b30c2525de4b49c2acab6f29
WIl K Edwards <edwardswk@protonmail.com>
2023-03-01 16:14:10 +0000

    Correctly app name

--- pong-clone/main.py
+++ pong-clone/main.py
@@ -6,7 +6,7 @@ import time
 
 def main():
     tk = Tk()
-    tk.title('Game')
+    tk.title('Pong Game')
     tk.resizable(0, 0)
     tk.wm_attributes('-topmost', 1)
     canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
